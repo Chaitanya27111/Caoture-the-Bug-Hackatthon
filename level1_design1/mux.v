9e20873a8de6bbc1c2873b44869b308fc9b89045
Chaitanya27111 <chaitanya27111@gmail.com>
2022-07-31 10:52:31 +0000

    bug

--- level1_design1/mux.v
+++ level1_design1/mux.v
@@ -38,7 +38,7 @@ module mux(sel,inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8,
       5'b01001: out = inp9;  
       5'b01010: out = inp10;
       5'b01011: out = inp11;
-      5'b01101: out = inp12; // bug===>missed 12, modify it to 1110
+      5'b01101: out = inp12; // bug===>missed 12, modify it to 01100
       5'b01101: out = inp13; 
       5'b01110: out = inp14;
       5'b01111: out = inp15;
