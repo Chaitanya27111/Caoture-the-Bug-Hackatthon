0f456ce26d3d4a10b6a1651b7a0252daf2c7b2c8
Chaitanya27111 <chaitanya27111@gmail.com>
2022-07-31 11:07:27 +0000

    done final

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -45,12 +45,7 @@ async def test_mux(dut):
         
         await Timer(2, units='ns')
         cocotb.log.info(f'##### sel = {dut.sel.value} ########')
-        '''l = [dut.inp0.value, dut.inp1.value, dut.inp2.value, dut.inp3.value, dut.inp4.value, 
-            dut.inp5.value, dut.inp6.value, dut.inp7.value, dut.inp8.value, dut.inp9.value, 
-            dut.inp10.value, dut.inp11.value, dut.inp12.value, dut.inp13.value, dut.inp14.value, 
-            dut.inp15.value, dut.inp16.value, dut.inp17.value, dut.inp18.value, dut.inp19.value, 
-            dut.inp20.value, dut.inp21.value, dut.inp22.value, dut.inp23.value, dut.inp24.value, 
-            dut.inp25.value, dut.inp26.value, dut.inp27.value, dut.inp28.value, dut.inp29.value, dut.inp30.value]'''
+        
         dut._log.info(f'i={i}')
         dut._log.info(f'Inpt={dut.inp10}')
 
