09f109f22143e5d07331d311fa47b38da8546087
Chaitanya27111 <84698480+Chaitanya27111@users.noreply.github.com>
2022-07-31 20:46:10 +0530

    found 3rd bug

--- level1_design2/seq_detect_1011.v
+++ level1_design2/seq_detect_1011.v
@@ -66,7 +66,7 @@ module seq_detect_1011(seq_seen, inp_bit, reset, clk);
       end
       SEQ_1011:
       begin
-        next_state = IDLE;
+        next_state = IDLE; //bug===>next state must be SEQ_10 for overlapping sequence detector
       end
     endcase
   end
