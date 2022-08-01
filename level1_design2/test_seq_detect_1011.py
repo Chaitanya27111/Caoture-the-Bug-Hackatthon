7ee64c6f5884f22172d112d482de9639b5d8871c
Chaitanya27111 <84698480+Chaitanya27111@users.noreply.github.com>
2022-08-01 13:04:03 +0530

    Delayed input bits

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, FallingEdge
+from cocotb.triggers import RisingEdge, FallingEdge, Timer
 
 @cocotb.test()
 async def test_seq_bug1(dut):
@@ -25,9 +25,14 @@ async def test_seq_bug1(dut):
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
     
-    inp_seq = [1, 1, 0, 1, 1]
-    
+    inp_seq = [1, 0, 1, 1]
+    #inp_seq = [1, 0, 1, 1, 1, 0, 1, 1]
+    #inp_seq = [1, 1, 0, 1, 1]
+    #inp_seq = [1, 1, 0, 1, 1]
+    #inp_seq = [1, 1, 0, 1, 1]
+    await RisingEdge(dut.clk)
     for i in range (0, len(inp_seq)-1):
+        await Timer(3, units="us")
         dut.inp_bit.value = inp_seq[i]
         await RisingEdge(dut.clk)
         cocotb.log.info("----------------------")
@@ -45,8 +50,7 @@ async def test_seq_bug1(dut):
     cocotb.log.info(f"Next state = {dut.next_state.value}")
     await RisingEdge(dut.clk)
     cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
-
-    
-    
+    cocotb.log.info(f"Current state = {dut.current_state.value}")
+    cocotb.log.info(f"Next state = {dut.next_state.value}")
 
     assert dut.seq_seen.value==1, f"Failed testcase for input sequence {inp_seq}"
