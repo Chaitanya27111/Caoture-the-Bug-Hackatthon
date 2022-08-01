193b6ade0fa8061bc3fa34478e113f0921772bf8
Chaitanya27111 <84698480+Chaitanya27111@users.noreply.github.com>
2022-08-01 12:41:37 +0530

    using for loop

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -25,57 +25,20 @@ async def test_seq_bug1(dut):
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
     
-    inp_seq = []
-    '''
-    dut.inp_bit.value=  0
+    inp_seq = [1, 1, 0, 1, 1]
+    
+    for i in range (0, len(inp_seq)-1):
+        dut.inp_bit.value = inp_seq[i]
+        await RisingEdge(dut.clk)
+        cocotb.log.info("----------------------")
+        cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
+        cocotb.log.info(f"Current state = {dut.current_state.value}")
+        cocotb.log.info(f"Next state = {dut.next_state.value}")
+        #await RisingEdge(dut.clk)
+        cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
+    
+    dut.inp_bit.value=  inp_seq[-1]
     await RisingEdge(dut.clk)
-    inp_seq.append(dut.inp_bit.value)
-    cocotb.log.info("----------------------")
-    cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
-    cocotb.log.info(f"Current state = {dut.current_state.value}")
-    cocotb.log.info(f"Next state = {dut.next_state.value}")
-    cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
-    dut.inp_bit.value=  1
-    await RisingEdge(dut.clk)
-    inp_seq.append(dut.inp_bit.value)
-    cocotb.log.info("----------------------")
-    cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
-    cocotb.log.info(f"Current state = {dut.current_state.value}")
-    cocotb.log.info(f"Next state = {dut.next_state.value}")
-    cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
-    '''
-    #for i in range (0, len(inp_seq)-1):
-
-    dut.inp_bit.value=  1
-    await RisingEdge(dut.clk)
-    inp_seq.append(dut.inp_bit.value)
-    cocotb.log.info("----------------------")
-    cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
-    cocotb.log.info(f"Current state = {dut.current_state.value}")
-    cocotb.log.info(f"Next state = {dut.next_state.value}")
-    #await RisingEdge(dut.clk)
-    cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
-    dut.inp_bit.value=  0
-    await RisingEdge(dut.clk)
-    inp_seq.append(dut.inp_bit.value)
-    cocotb.log.info("----------------------")
-    cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
-    cocotb.log.info(f"Current state = {dut.current_state.value}")
-    cocotb.log.info(f"Next state = {dut.next_state.value}")
-    #await RisingEdge(dut.clk)
-    cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
-    dut.inp_bit.value=  1
-    await RisingEdge(dut.clk)
-    inp_seq.append(dut.inp_bit.value)
-    cocotb.log.info("----------------------")
-    cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
-    cocotb.log.info(f"Current state = {dut.current_state.value}")
-    cocotb.log.info(f"Next state = {dut.next_state.value}")
-    #await RisingEdge(dut.clk)
-    cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
-    dut.inp_bit.value=  1
-    await RisingEdge(dut.clk)
-    inp_seq.append(dut.inp_bit.value)
     cocotb.log.info("----------------------")
     cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
     cocotb.log.info(f"Current state = {dut.current_state.value}")
@@ -84,23 +47,6 @@ async def test_seq_bug1(dut):
     cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
 
     
-    '''
-    dut.inp_bit.value=  0
-    await RisingEdge(dut.clk)
-    inp_seq.append(dut.inp_bit.value)
-    cocotb.log.info("----------------------")
-    cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
-    cocotb.log.info(f"Current state = {dut.current_state.value}")
-    cocotb.log.info(f"Next state = {dut.next_state.value}")
-    cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
-    dut.inp_bit.value=  1
-    await RisingEdge(dut.clk)
-    inp_seq.append(dut.inp_bit.value)
-    cocotb.log.info("----------------------")
-    cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
-    cocotb.log.info(f"Current state = {dut.current_state.value}")
-    cocotb.log.info(f"Next state = {dut.next_state.value}")
-    cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
-    '''
+    
 
     assert dut.seq_seen.value==1, f"Failed testcase for input sequence {inp_seq}"
