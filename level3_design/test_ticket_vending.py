c70ccced49840ce40d55ae91fb123d4acad131c8
Chaitanya27111 <chaitanya27111@gmail.com>
2022-08-01 14:45:46 +0000

    bugfree design tb

--- level3_design/test_ticket_vending.py
+++ level3_design/test_ticket_vending.py
@@ -26,22 +26,33 @@ async def test_seq_bug1(dut):
     cocotb.log.info('#### CTB: Develop your test here! ######')
     
     #inp_seq = [1, 1, 0, 1, 1, 1]
-    inp_seq = [1, 0, 0, 1, 0, 1,]
+    inp_seq = [1, 0, 0, 1, 1, 0]
     
     await RisingEdge(dut.Clock)
     for i in range (0, len(inp_seq), 2):
         cocotb.log.info("----------------------")
-        await Timer(3, units="us")
+        #await Timer(3, units="us")
         dut.Ten.value = inp_seq[i]
         dut.Twenty.value = inp_seq[i+1]
+        await RisingEdge(dut.Clock)
+        
         cocotb.log.info(f"Output bit Ready = {dut.Ready.value}")
         cocotb.log.info(f"Output bit Dispense = {dut.Dispense.value}")
         cocotb.log.info(f"Output bit Return = {dut.Return.value}")
         cocotb.log.info(f"Output bit Bill = {dut.Bill.value}")
-        await RisingEdge(dut.Clock)
+        
         cocotb.log.info(f"Ten = {dut.Ten.value}")
         cocotb.log.info(f"Twenty = {dut.Twenty.value}")
         cocotb.log.info(f"Current state = {dut.State.value}")
         cocotb.log.info(f"Next state = {dut.NextState.value}")
         
-    #assert dut.seq_seen.value==1, f"Failed testcase for input sequence {inp_seq}"
+    await RisingEdge(dut.Clock)
+    cocotb.log.info("----------------------")
+    cocotb.log.info(f"Output bit Ready = {dut.Ready.value}")
+    cocotb.log.info(f"Output bit Dispense = {dut.Dispense.value}")
+    cocotb.log.info(f"Output bit Return = {dut.Return.value}")
+    cocotb.log.info(f"Output bit Bill = {dut.Bill.value}")
+    cocotb.log.info(f"Output bit Dispense = {dut.Dispense.value}")
+    cocotb.log.info(f"Current state = {dut.State.value}")
+    cocotb.log.info(f"Next state = {dut.NextState.value}")
+    assert dut.Dispense.value==1, f"Failed testcase for input sequence {inp_seq}"
