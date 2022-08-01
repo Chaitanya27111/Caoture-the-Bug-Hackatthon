3cba8621852e463c87de1c3f9acc6aa0a2692d7a
Chaitanya27111 <84698480+Chaitanya27111@users.noreply.github.com>
2022-08-01 12:36:32 +0530

    updated

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -24,3 +24,83 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
+    
+    inp_seq = []
+    '''
+    dut.inp_bit.value=  0
+    await RisingEdge(dut.clk)
+    inp_seq.append(dut.inp_bit.value)
+    cocotb.log.info("----------------------")
+    cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
+    cocotb.log.info(f"Current state = {dut.current_state.value}")
+    cocotb.log.info(f"Next state = {dut.next_state.value}")
+    cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
+    dut.inp_bit.value=  1
+    await RisingEdge(dut.clk)
+    inp_seq.append(dut.inp_bit.value)
+    cocotb.log.info("----------------------")
+    cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
+    cocotb.log.info(f"Current state = {dut.current_state.value}")
+    cocotb.log.info(f"Next state = {dut.next_state.value}")
+    cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
+    '''
+    #for i in range (0, len(inp_seq)-1):
+
+    dut.inp_bit.value=  1
+    await RisingEdge(dut.clk)
+    inp_seq.append(dut.inp_bit.value)
+    cocotb.log.info("----------------------")
+    cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
+    cocotb.log.info(f"Current state = {dut.current_state.value}")
+    cocotb.log.info(f"Next state = {dut.next_state.value}")
+    #await RisingEdge(dut.clk)
+    cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
+    dut.inp_bit.value=  0
+    await RisingEdge(dut.clk)
+    inp_seq.append(dut.inp_bit.value)
+    cocotb.log.info("----------------------")
+    cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
+    cocotb.log.info(f"Current state = {dut.current_state.value}")
+    cocotb.log.info(f"Next state = {dut.next_state.value}")
+    #await RisingEdge(dut.clk)
+    cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
+    dut.inp_bit.value=  1
+    await RisingEdge(dut.clk)
+    inp_seq.append(dut.inp_bit.value)
+    cocotb.log.info("----------------------")
+    cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
+    cocotb.log.info(f"Current state = {dut.current_state.value}")
+    cocotb.log.info(f"Next state = {dut.next_state.value}")
+    #await RisingEdge(dut.clk)
+    cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
+    dut.inp_bit.value=  1
+    await RisingEdge(dut.clk)
+    inp_seq.append(dut.inp_bit.value)
+    cocotb.log.info("----------------------")
+    cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
+    cocotb.log.info(f"Current state = {dut.current_state.value}")
+    cocotb.log.info(f"Next state = {dut.next_state.value}")
+    await RisingEdge(dut.clk)
+    cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
+
+    
+    '''
+    dut.inp_bit.value=  0
+    await RisingEdge(dut.clk)
+    inp_seq.append(dut.inp_bit.value)
+    cocotb.log.info("----------------------")
+    cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
+    cocotb.log.info(f"Current state = {dut.current_state.value}")
+    cocotb.log.info(f"Next state = {dut.next_state.value}")
+    cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
+    dut.inp_bit.value=  1
+    await RisingEdge(dut.clk)
+    inp_seq.append(dut.inp_bit.value)
+    cocotb.log.info("----------------------")
+    cocotb.log.info(f"Input bit = {dut.inp_bit.value}")
+    cocotb.log.info(f"Current state = {dut.current_state.value}")
+    cocotb.log.info(f"Next state = {dut.next_state.value}")
+    cocotb.log.info(f"Output bit = {dut.seq_seen.value}")
+    '''
+
+    assert dut.seq_seen.value==1, f"Failed testcase for input sequence {inp_seq}"
